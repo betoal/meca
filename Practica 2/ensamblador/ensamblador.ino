872bdb1bce9b109b4bae493787fde9210aa0b506
Edgar Granados <edgar.granados@itam.mx>
2019-02-08 11:01:20 -0600

    Practica 2 C

--- Practica 2/ensamblador/ensamblador.ino
+++ Practica 2/ensamblador/ensamblador.ino
@@ -1,18 +1,43 @@
 #include <math.h>
 
+bool isBlinking = false;
+int pinB7 = 13;
+int pinButton = 10;
+unsigned int cont = 0;
+
 void setup()
 {
   Serial.begin(9600);
   
-  // put your setup code here, to run once:
+  // Sets the ports
+  DDRB = DDRB | B10000000;
+  //pinMode(pinB7, OUTPUT);
+  pinMode(pinButton, INPUT);
 
+  // Timer
+  cli();
+  TCCR1B = 0;
+  TCCR1A = 0;
+  TCCR1B |= (1 << CS12);
+  TCNT1 = 3036;
+  TIMSK1 |= (1 << TOIE1);
+  sei();
+
+  // Runs functions
   Serial.println("Determinante");
   Serial.println(getDeterminant(10, 20, 30));
   Serial.println(getDeterminantAssembly(10, 20, 30));
   Serial.println("Promedio");
   int a[] = {1, 2, 3, 4};
   Serial.println(getAverage(a, 4));
-  Serial.println(getAverageAssembly(a, 4));
+  //Serial.println(getAverageAssembly(a, 4));
+  //blink();
+}
+
+ISR(TIMER1_OVF_vect)
+{
+  //isBlinking = !isBlinking;
+  //blinkAssembly(isBlinking);
 }
 
 /**
@@ -64,13 +89,14 @@ int getAverage(int n[], int size)
 
 int getAverageAssembly(int n[], int size)
 {
+  /**
   int res = 0;
   asm volatile(
     "MOV r18, 0x00 \n\t" // Sum
     "MOV r16, %1  \n\t" // size
     "MOV r17, 0x00 \n\t" // i
     "FOR: \n\t"
-    "ADD r18, (%2) \n\t"
+    "ADD r18, %2 \n\t"
     "ADD %2, 0x2 \n\t"
     "INC r17 \n\t" // Increments
     "CPSE r16, r17 \n\t" // Compares
@@ -88,9 +114,62 @@ int getAverageAssembly(int n[], int size)
   );
   
   return res;
+  **/
+  return 0;
+}
+
+void blinkAssembly(bool bl)
+{
+  asm volatile(
+    "LDI r16, 0x00 \n\t"
+    "CPSE r16, %2 \n\t"
+    "JMP OFF \n\t"
+    "JMP ON \n\t"
+    "OFF: CBI %0, %1 \n\t" // off
+    "JMP EXIT \n\t"
+    "ON: SBI %0, %1 \n\t" // on
+    "EXIT: \n\t"
+    :: "I" (_SFR_IO_ADDR(PORTB)), "I" (PORTB7), "r" (bl)
+  );
+}
+
+void buttonAssembly()
+{
+  asm volatile(
+    "SBI %0, %1 \n\t"
+    "SBIS %2, %3 \n\t"
+    "CBI %0, %1 \n\t"
+    :: "I" (_SFR_IO_ADDR(PORTB)), "I" (PORTB7), "I" (_SFR_IO_ADDR(PINB)), "I" (PINB5)
+  );
+  
+}
+
+void blink()
+{
+  while(true)
+  {
+    digitalWrite(pinB7, HIGH);
+    delay(250);
+    digitalWrite(pinB7, LOW);
+    delay(250);
+  }
+}
+
+void button()
+{
+  if (digitalRead(pinButton))
+  {
+    digitalRead(pinButton);
+    cont++;
+    Serial.print("Counter: ");
+    Serial.print(cont);
+    Serial.print('\n');
+  }
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
+  //buttonAssembly();
+  button();
 }
